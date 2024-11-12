a7014f5ea743e014156521e6c0d8e826ce3b719d
Julien Motaharian <motaharian.j@northeastern.edu>
2024-11-11 20:41:33 -0500

    Added comments

--- src/cs3500/threetrios/model/ReadonlyThreeTriosModel.java
+++ src/cs3500/threetrios/model/ReadonlyThreeTriosModel.java
@@ -4,6 +4,9 @@ import cs3500.threetrios.grid.GridTile;
 import cs3500.threetrios.player.Player;
 import cs3500.threetrios.player.PlayerColor;
 
+/**
+ * A "read only" model interface holding all the methods that do not mutate any data.
+ */
 public interface ReadonlyThreeTriosModel {
 
   boolean isGameOver();
