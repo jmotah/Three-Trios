4bfde9787ea464ed8a583cdf7e884b0e3eea5e0a
Julien Motaharian <motaharian.j@northeastern.edu>
2024-11-12 16:46:15 -0500

    source organization

--- src/cs3500/threetrios/model/ReadonlyThreeTriosModel.java
+++ src/cs3500/threetrios/model/ReadonlyThreeTriosModel.java
@@ -1,8 +1,8 @@
 package cs3500.threetrios.model;
 
-import cs3500.threetrios.grid.GridTile;
-import cs3500.threetrios.player.Players;
-import cs3500.threetrios.player.PlayerColor;
+import cs3500.threetrios.model.grid.GridTile;
+import cs3500.threetrios.model.player.Players;
+import cs3500.threetrios.model.player.PlayerColor;
 
 /**
  * A "read only" model interface holding all the methods that do not mutate any data.
