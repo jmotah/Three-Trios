114ebe0df2127a02d8e663691615a9898cd5ea37
Julien Motaharian <motaharian.j@northeastern.edu>
2024-11-13 13:57:25 -0500

    Changes for testing

--- src/cs3500/threetrios/ThreeTriosGame.java
+++ src/cs3500/threetrios/ThreeTriosGame.java
@@ -1,9 +1,15 @@
 package cs3500.threetrios;
 
+import java.awt.*;
 import java.io.File;
+import java.util.HashMap;
 
 import cs3500.threetrios.model.ThreeTriosModel;
 import cs3500.threetrios.model.playervscomputer.PlayerComputerModel;
+import cs3500.threetrios.model.playervsplayer.PlayerPlayerModel;
+import cs3500.threetrios.model.strategies.Strategies;
+import cs3500.threetrios.model.strategies.Strategy1;
+import cs3500.threetrios.model.strategies.Strategy2;
 import cs3500.threetrios.view.ThreeTriosView;
 import cs3500.threetrios.view.graphical.GraphicalView;
 
@@ -18,7 +24,7 @@ public class ThreeTriosGame {
    * @param args command-line arguments
    */
   public static void main(String[] args) {
-    ThreeTriosModel model = new PlayerComputerModel();
+    ThreeTriosModel model = new PlayerPlayerModel();
 
     File cardConfig = new File(
             "/Users/julienmotaharian/Desktop/OOD Projects/Group Projects/ThreeTriosBetter/src/" +
@@ -30,14 +36,20 @@ public class ThreeTriosGame {
 
     model.startGame(cardConfig, gridConfig);
 
-    model.playToGridAI();
+    Strategy2 strategy = new Strategy2(model);
 
     ThreeTriosView view = new GraphicalView(model);
     view.makeVisible();
+  }
 
-    model.playToGridAI();
-    view.refresh();
-    model.playToGridAI();
-    view.refresh();
+  public static void playToGridAndBattleWithOptimalMove(HashMap<Point, Integer> optimalMove,
+                                                        ThreeTriosModel model) {
+    Point position = optimalMove.keySet().iterator().next();
+    int x = (int) position.getX();
+    int y = (int) position.getY();
+    int cardIdx = optimalMove.get(position);
+
+    model.playToGrid(x, y, cardIdx);
+    model.battle(x, y);
   }
 }
