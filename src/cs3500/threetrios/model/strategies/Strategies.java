f232e129ea1293d76403c0e59ceaa617e6bc0ecf
Julien Motaharian <motaharian.j@northeastern.edu>
2024-11-13 14:22:56 -0500

    runStrategies to runStrategy

--- src/cs3500/threetrios/model/strategies/Strategies.java
+++ src/cs3500/threetrios/model/strategies/Strategies.java
@@ -3,11 +3,8 @@ package cs3500.threetrios.model.strategies;
 import java.awt.*;
 import java.util.HashMap;
 
-import cs3500.threetrios.model.ReadonlyThreeTriosModel;
-import cs3500.threetrios.model.ThreeTriosModel;
-
 public interface Strategies {
 
-  HashMap<Point, Integer> runStrategies();
+  HashMap<Point, Integer> runStrategy();
 
 }
