e604b2f36b0d82dd8cbf67f99ed1dc79ff73a658
Julien Motaharian <motaharian.j@northeastern.edu>
2024-12-04 14:24:41 -0500

    removed unnecessary adapter classes and added comments

--- src/cs3500/threetrios/providers/model/object_adapters/MainModelToProviderReadonlyThreeTrios.java
+++ src/cs3500/threetrios/providers/model/object_adapters/MainModelToProviderReadonlyThreeTrios.java
@@ -10,10 +10,20 @@ import cs3500.threetrios.providers.model.Player;
 import cs3500.threetrios.providers.model.PlayerColor;
 import cs3500.threetrios.providers.model.ReadonlyThreeTriosModel;
 
+/**
+ * Adapts the project's main ReadonlyThreeTriosModel interface and class implementation to the
+ * provider's ReadonlyThreeTriosModel interface. Adapts the primary implementation to a provider's
+ * implementation.
+ */
 public class MainModelToProviderReadonlyThreeTrios implements ReadonlyThreeTriosModel {
 
   private final cs3500.threetrios.model.ReadonlyThreeTriosModel model;
 
+  /**
+   * MainModelToProviderReadonlyThreeTrios class constructor.
+   *
+   * @param model the main ReadonlyThreeTriosModel interface implementation class to delegate to
+   */
   public MainModelToProviderReadonlyThreeTrios(
           cs3500.threetrios.model.ReadonlyThreeTriosModel model) {
 
@@ -23,16 +33,31 @@ public class MainModelToProviderReadonlyThreeTrios implements ReadonlyThreeTrios
     this.model = model;
   }
 
+  /**
+   * Gets the current grid of the game by delegating.
+   *
+   * @return the grid representing the game state.
+   */
   @Override
   public Grid getGrid() {
     return new MainGridToProviderGrid(model.getGrid());
   }
 
+  /**
+   * Gets the current player by delegating.
+   *
+   * @return the player whose turn it is.
+   */
   @Override
   public Player getCurrentPlayer() {
     return new MainHumanToProviderHuman(model.getCurrentTurnPlayer());
   }
 
+  /**
+   * Gets the player who is not currently taking their turn by delegating.
+   *
+   * @return the player whose turn it is not, aka the opponent of the current player.
+   */
   @Override
   public Player getOtherPlayer() {
     cs3500.threetrios.model.player.PlayerColor otherPlayerColor;
@@ -49,11 +74,23 @@ public class MainModelToProviderReadonlyThreeTrios implements ReadonlyThreeTrios
             model.getPlayerOfColor(otherPlayerColor));
   }
 
+  /**
+   * Checks if the game is over by delegating.
+   *
+   * @return true if the game is over, false otherwise.
+   */
   @Override
   public boolean isGameOver() {
     return model.isGameOver();
   }
 
+  /**
+   * Gets the contents of a cell in the grid at a given position by delegating.
+   *
+   * @param row the row of the cell.
+   * @param col the column of the cell.
+   * @return the contents of the cell at the given position.
+   */
   @Override
   public Cell getCell(int row, int col) {
     if (row < 0 || row >= model.getGrid().length ||
@@ -64,6 +101,13 @@ public class MainModelToProviderReadonlyThreeTrios implements ReadonlyThreeTrios
     return new MainGridTileToProviderCell(model.getGrid()[row][col]);
   }
 
+  /**
+   * Checks if the current player can legally play a card at the given coordinates by delegating.
+   *
+   * @param row the row of the cell.
+   * @param col the column of the cell.
+   * @return true if the current player can place a card at the specified cell, false otherwise.
+   */
   @Override
   public boolean canCurrentPlayerPlaceCard(int row, int col) {
     if (row < 0 || row >= model.getGrid().length ||
@@ -79,6 +123,13 @@ public class MainModelToProviderReadonlyThreeTrios implements ReadonlyThreeTrios
     return false;
   }
 
+  /**
+   * Gets the score of a player, which is the number of cards they own in their hand
+   * plus the number of cards they own on the grid by delegating.
+   *
+   * @param player the player whose score to retrieve.
+   * @return the score of the player.
+   */
   @Override
   public int getPlayerScore(Player player) {
     if (player == null) {
@@ -95,6 +146,12 @@ public class MainModelToProviderReadonlyThreeTrios implements ReadonlyThreeTrios
     }
   }
 
+  /**
+   * Gets the list of cards in a player's hand by delegating.
+   *
+   * @param player the player whose hand to retrieve.
+   * @return the list of cards in the player's hand.
+   */
   @Override
   public List<Card> getPlayerHand(Player player) {
     if (player == null) {
@@ -104,11 +161,22 @@ public class MainModelToProviderReadonlyThreeTrios implements ReadonlyThreeTrios
     return player.getHand();
   }
 
+  /**
+   * Determines the winner of the game by delegating.
+   *
+   * @return the Player who has won, or null if there is no winner yet.
+   */
   @Override
   public Player determineWinner() {
     return new MainHumanToProviderHuman(model.findWinningPlayer());
   }
 
+  /**
+   * Finds the number of cells that have placed cards on them within the grid.
+   *
+   * @param grid the grid object to look at
+   * @return the number of cells that have placed cards on them within the grid
+   */
   private int findNumberOfPlacedCardsOnGrid(Grid grid) {
     if (grid == null) {
       throw new IllegalArgumentException("grid cannot be null");
@@ -118,7 +186,8 @@ public class MainModelToProviderReadonlyThreeTrios implements ReadonlyThreeTrios
 
     for (int row = 0; row < grid.getRows(); row++) {
       for (int column = 0; column < grid.getCols(); column++) {
-        if (!grid.getCell(row, column).isHole()) {
+        if (!grid.getCell(row, column).isHole() &&
+                grid.getCell(row, column).getCard() != null) {
           placedCardCount++;
         }
       }
--- src/cs3500/threetrios/providers/model/object_adapters/ProviderFeaturesToMainFeatures.java
+++ src/cs3500/threetrios/providers/model/object_adapters/ProviderFeaturesToMainFeatures.java
@@ -3,10 +3,20 @@ package cs3500.threetrios.providers.model.object_adapters;
 import cs3500.threetrios.controller.Features;
 import cs3500.threetrios.providers.controller.ThreeTriosFeatures;
 
+/**
+ * Adapts the provider's ThreeTriosFeatures interface and class implementation to the project's
+ * main Features interface. Adapts the provider's implementation to the primary interface.
+ */
 public class ProviderFeaturesToMainFeatures implements Features {
 
   private final ThreeTriosFeatures providerFeatures;
 
+  /**
+   * ProviderFeaturesToMainFeatures class constructor.
+   *
+   * @param providerFeatures the provider ThreeTriosFeatures interface implementation class to
+   *                         delegate to
+   */
   public ProviderFeaturesToMainFeatures(ThreeTriosFeatures providerFeatures) {
     if (providerFeatures == null) {
       throw new IllegalArgumentException("providerFeatures cannot be null!");
@@ -14,13 +24,26 @@ public class ProviderFeaturesToMainFeatures implements Features {
     this.providerFeatures = providerFeatures;
   }
 
+  /**
+   * Given a card index, selects the respective card in the player's hand by delegating.
+   *
+   * @param cardIndex the card index of the card to select
+   */
   @Override
   public void selectCard(int cardIndex) {
     this.providerFeatures.selectCard(cardIndex);
   }
 
+  /**
+   * Given a row and column index, selects the respective grid tile in the game grid and places the
+   * selected card index there. The requirement to use this method is that there must be a selected
+   * card index already.
+   *
+   * @param row    the row index to play to
+   * @param column the column index to play to
+   */
   @Override
   public void selectGridCell(int row, int column) {
-    this.selectGridCell(row, column);
+    providerFeatures.selectCell(row, column);
   }
 }
